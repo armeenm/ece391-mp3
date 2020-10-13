836806633bf742ee4fb5dbe864f3d06f7cb7e6b8
Armeen Mahdian <mahdianarmeen@gmail.com>
2020-10-13 09:13:16 -0500

    Add additional gitignore entry, add clang-format

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -1,27 +1,26 @@
 #include "tests.h"
-#include "x86_desc.h"
 #include "lib.h"
+#include "x86_desc.h"
 
 #define PASS 1
 #define FAIL 0
 
 /* format these macros as you see fit */
-#define TEST_HEADER 	\
-	printf("[TEST %s] Running %s at %s:%d\n", __FUNCTION__, __FUNCTION__, __FILE__, __LINE__)
-#define TEST_OUTPUT(name, result)	\
-	printf("[TEST %s] Result = %s\n", name, (result) ? "PASS" : "FAIL");
+#define TEST_HEADER                                                                                \
+  printf("[TEST %s] Running %s at %s:%d\n", __FUNCTION__, __FUNCTION__, __FILE__, __LINE__)
+#define TEST_OUTPUT(name, result)                                                                  \
+  printf("[TEST %s] Result = %s\n", name, (result) ? "PASS" : "FAIL");
 
-static inline void assertion_failure(){
-	/* Use exception #15 for assertions, otherwise
-	   reserved by Intel */
-	asm volatile("int $15");
+static inline void assertion_failure() {
+  /* Use exception #15 for assertions, otherwise
+     reserved by Intel */
+  asm volatile("int $15");
 }
-
 
 /* Checkpoint 1 tests */
 
 /* IDT Test - Example
- * 
+ *
  * Asserts that first 10 IDT entries are not NULL
  * Inputs: None
  * Outputs: PASS/FAIL
@@ -29,20 +28,19 @@ static inline void assertion_failure(){
  * Coverage: Load IDT, IDT definition
  * Files: x86_desc.h/S
  */
-int idt_test(){
-	TEST_HEADER;
+int idt_test() {
+  TEST_HEADER;
 
-	int i;
-	int result = PASS;
-	for (i = 0; i < 10; ++i){
-		if ((idt[i].offset_15_00 == NULL) && 
-			(idt[i].offset_31_16 == NULL)){
-			assertion_failure();
-			result = FAIL;
-		}
-	}
+  int i;
+  int result = PASS;
+  for (i = 0; i < 10; ++i) {
+    if ((idt[i].offset_15_00 == NULL) && (idt[i].offset_31_16 == NULL)) {
+      assertion_failure();
+      result = FAIL;
+    }
+  }
 
-	return result;
+  return result;
 }
 
 // add more tests here
@@ -52,9 +50,8 @@ int idt_test(){
 /* Checkpoint 4 tests */
 /* Checkpoint 5 tests */
 
-
 /* Test suite entry point */
-void launch_tests(){
-	TEST_OUTPUT("idt_test", idt_test());
-	// launch your tests here
+void launch_tests() {
+  TEST_OUTPUT("idt_test", idt_test());
+  // launch your tests here
 }
--- syscalls/ece391support.h
+++ syscalls/ece391support.h
@@ -6,8 +6,8 @@ extern void ece391_strcpy(uint8_t* dst, const uint8_t* src);
 extern void ece391_fdputs(int32_t fd, const uint8_t* s);
 extern int32_t ece391_strcmp(const uint8_t* s1, const uint8_t* s2);
 extern int32_t ece391_strncmp(const uint8_t* s1, const uint8_t* s2, uint32_t n);
-extern uint8_t *ece391_itoa(uint32_t value, uint8_t* buf, int32_t radix);
-extern uint8_t *ece391_strrev(uint8_t* s);
+extern uint8_t* ece391_itoa(uint32_t value, uint8_t* buf, int32_t radix);
+extern uint8_t* ece391_strrev(uint8_t* s);
 
 #endif /* ECE391SUPPORT_H */
 
