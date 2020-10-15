2564b05a29bdd8a490ab3b34ec93b306fbc69e1a
Armeen Mahdian <mahdianarmeen@gmail.com>
2020-10-15 08:29:27 -0500

    [Keyboard] Add loop to print scancode

--- student-distrib/keyboard.c
+++ student-distrib/keyboard.c
@@ -5,11 +5,14 @@
 void init_keyboard(void) { enable_irq(KEYBOARD_IRQ); }
 
 void keyboard_irqh(void) {
+  cli();
   disable_irq(KEYBOARD_IRQ);
   send_eoi(KEYBOARD_IRQ);
   sti();
 
-  printf("Testing...\n");
+  while (inb(KEYBOARD_STATUS_PORT) & KEYBOARD_OUTBUF_FULL) {
+    printf("Keyboard: %d\n", inb(KEYBOARD_DATA_PORT));
+  }
 
   enable_irq(KEYBOARD_IRQ);
 }
