910ecaab2fdeec0da28cc026bc4f32ec722ad8f7
braeden123 <braedensmith2@gmail.com>
2020-10-18 06:07:57 -0500

    [RTC] fix RTC by ACKing reg C, add consts

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -7,29 +7,37 @@ void init_rtc() {
 
   enable_irq(RTC_IRQ);
 
-  /* Save old values */
-  outb(0x8B, RTC_SEL_PORT);
-  prev = inb(RTC_DATA_PORT);
+  cli();
+  // Turn on periodic interupts
+  outb(RTC_REG_B | RTC_DIS_NMI, RTC_SEL_PORT); // Select register B
+  prev = inb(RTC_DATA_PORT); // Read it's old value (we may be able to drop this reliably since we own init)
 
-  outb(0x8B, RTC_SEL_PORT);
-  outb(prev | 0x40, RTC_DATA_PORT);
+  outb(RTC_REG_B | RTC_DIS_NMI, RTC_SEL_PORT); // Reselect register B to avoid reseting index
+  outb(prev | BIT_6_MASK, RTC_DATA_PORT); // turn on bit 6 of reg B for periodic ints enable (PIE)
 
-  set_freq_rtc(HZ1024);
+  set_freq_rtc(HZ2);
+  sti(); // we may want to renable NMI's here, see OSDEV
 }
 
 void irqh_rtc() {
   send_eoi(RTC_IRQ);
   sti();
 
-  printf("RTC Event\n");
+  printf("RTC Event \n");
+  ack_rtc_int();
+}
+
+char ack_rtc_int() {
+  outb(RTC_REG_C, RTC_SEL_PORT); // We need to read register C to permit multiple interupts
+  return inb(RTC_DATA_PORT); // if we want, this gives us back int details
 }
 
 void set_freq_rtc(RTCFreq const freq) {
   uint8_t prev;
 
-  outb(0x8A, RTC_SEL_PORT);
-  prev = inb(RTC_DATA_PORT);
+  outb(RTC_REG_A | RTC_DIS_NMI, RTC_SEL_PORT);
+  prev = inb(RTC_DATA_PORT); // pull the old value of reg A
 
-  outb(0x8A, RTC_SEL_PORT);
-  outb((prev & 0xF0) | (uint8_t)freq, RTC_DATA_PORT);
+  outb(RTC_REG_A | RTC_DIS_NMI, RTC_SEL_PORT); // reselect reg A
+  outb((prev & 0xF0) | (uint8_t)freq, RTC_DATA_PORT); // set rate in the bottom 4 bits
 }
--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -5,10 +5,20 @@
 #define RTC_DATA_PORT 0x71
 #define RTC_IRQ 0x8
 
+#define RTC_REG_A 0x0A
+#define RTC_REG_B 0x0B
+#define RTC_REG_C 0x0C
+// http://www.walshcomptech.com/ohlandl/config/cmos_bank_0.html#Hex_000
+#define BIT_6_MASK 1<<6 // PIE  The period-interrupt-enable bit enables an interrupt to occur at a rate
+#define RTC_DIS_NMI 1<<7 // 0x80 (https://stackoverflow.com/questions/55394608/how-do-i-disable-non-maskable-interrupts-programmatically)
+
+
 typedef enum RTCFreq { HZ1024 = 0x6, HZ512, HZ256, HZ128, HZ64, HZ32, HZ16, HZ8, HZ4, HZ2 } RTCFreq;
 
 void init_rtc(void);
 void irqh_rtc(void);
 void set_freq_rtc(RTCFreq freq);
+char ack_rtc_int();
+
 
 #endif
