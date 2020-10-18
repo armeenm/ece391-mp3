5881ab9e3ca37d153ddd1633d04286d91a51ca47
Armeen Mahdian <mahdianarmeen@gmail.com>
2020-10-18 13:03:26 -0500

    [Tests] Some more IDT test improvements

--- student-distrib/tests.c
+++ student-distrib/tests.c
@@ -19,6 +19,13 @@ static inline void assertion_failure() {
 
 /* Checkpoint 1 tests */
 
+int idt_test_helper(int const i, int const reserved3, int const dpl) {
+  return (!idt[i].offset_15_00 && !idt[i].offset_31_16) || (idt[i].seg_selector != KERNEL_CS) ||
+         idt[i].reserved4 || (idt[i].reserved3 != reserved3) || !idt[i].reserved2 ||
+         !idt[i].reserved1 || !idt[i].size || idt[i].reserved0 || (idt[i].dpl != dpl) ||
+         !idt[i].present;
+}
+
 /* IDT Test
  *
  * Asserts that IDT exception entries are correct
@@ -28,23 +35,31 @@ static inline void assertion_failure() {
  * Coverage: Load IDT, IDT definition
  * Files: x86_desc.h/S
  */
+
 int idt_test() {
   int i;
   int result = PASS;
 
   TEST_HEADER;
 
-  for (i = 0; i < 31; ++i) {
-    if ((i > 20 && i < 30) || i == 15)
+  /* Check the first 20 entires, skipping 15 (reserved) */
+  for (i = 0; i < 21; ++i) {
+    if (i == 15)
       continue;
 
-    if ((!idt[i].offset_15_00 && !idt[i].offset_31_16) || idt[i].seg_selector != KERNEL_CS ||
-        idt[i].reserved4 || !idt[i].reserved3 || !idt[i].reserved2 || !idt[i].reserved1 ||
-        !idt[i].size || idt[i].reserved0 || idt[i].dpl || !idt[i].present) {
-
+    if (idt_test_helper(i, 1, 0)) {
       result = FAIL;
       assertion_failure();
     }
+  }
+
+  /* Only ones left are 30 (#SX), PIT, keyboard, RTC, and syscall entries */
+  if (idt_test_helper(30, 1, 0) || idt_test_helper(PIT_IDT, 0, 0) ||
+      idt_test_helper(KEYBOARD_IDT, 0, 0) || idt_test_helper(RTC_IDT, 0, 0) ||
+      idt_test_helper(SYSCALL_IDT, 0, 3)) {
+
+    result = FAIL;
+    assertion_failure();
   }
 
   return result;
